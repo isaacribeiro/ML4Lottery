af4f846a3ffe09831551f54580d448b77616a924
Isaac Ribeiro <ribeiro.isaac@hotmail.com>
2018-05-08 22:29:51 -0300

    Initial commit

---
+++ src/main/java/my/learning/Ml4LotteryApplication.java
@@ -0,0 +1,12 @@
+package my.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ml4LotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ml4LotteryApplication.class, args);
+	}
+}
